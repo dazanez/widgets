1355ff906e2d415518189ce62bf9684e8a6b49af
dazanez <dasl.672004@gmail.com>
2024-06-20 18:06:30 -0500

    App theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Otra forma de definir un arreglo
+const colors = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.indigo,
+  Colors.yellow,
+  Colors.lime,
+  Colors.cyanAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor < colors.length,
+            'There are only ${colors.length} colors, you can select up to ${colors.length - 1} color index');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colors[selectedColor],
+  );
+}
